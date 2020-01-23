0c82a64e0799a6139c3b098d989112e82ba722d8
Joshua Connor <jconno26@uic.edu>
2020-01-23 11:21:19 -0600

    database connected

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const layout = require("./views/layout");
+const { db } = require("./models");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res, next) => {
+  res.send(layout(""));
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
